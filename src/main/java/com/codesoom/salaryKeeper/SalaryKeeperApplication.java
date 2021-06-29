52ac30923c866ac1e612dd355e500adefb7844c3
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2021-06-29 22:49:21 +0900

    프로젝트 생성.
    - Spring initializr를 통한 프로젝트 생성.
    - 의존성 추가.
      - Spring Boot DevTools
      - Lombok
      - Spring Web
      - Spring Security
      - Spring Data JPA
      - H2 Database

---
+++ src/main/java/com/codesoom/salaryKeeper/SalaryKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.codesoom.salaryKeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryKeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codesoom/salaryKeeper/SalaryKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesoom.salaryKeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
