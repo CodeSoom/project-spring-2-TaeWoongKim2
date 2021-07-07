2e9212960b83869f1c0bd60c07c435a301341a0d
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2021-07-08 01:00:05 +0900

    User 도메인 및 JPA 레퍼지토리 구현.
    - Doamin: User
    - Repository: UserRepository
    - infra: JpaUserRepository

--- app/src/main/java/com/codesoom/project/core/application/UserService.java
+++ app/src/main/java/com/codesoom/project/core/application/UserService.java
@@ -1,2 +1,35 @@
-package com.codesoom.project.core.application;public class UserService {
+package com.codesoom.project.core.application;
+
+import com.codesoom.project.core.domain.User;
+import com.codesoom.project.core.domain.UserRepository;
+import com.codesoom.project.web.dto.UserRegistrationData;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+/**
+ * 사용자에 관한 유즈케이스를 담당합니다.
+ */
+@Service
+@Transactional
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * 사용자를 등록한다.
+     *
+     * @param registrationData 등록하기 위한 데이터
+     * @return 등록된 사용자
+     */
+    public User createUser(UserRegistrationData registrationData) {
+
+        User user = userRepository.save(
+                mapper.map(registrationData, User.class));
+
+        return user;
+    }
 }
