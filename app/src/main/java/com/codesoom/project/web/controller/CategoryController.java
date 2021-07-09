0a4308800d987747cf1607f5fd092d056e5f0b21
TaeWoongKim2 <ktaewoong46@gmail.com>
2021-07-09 15:10:11 +0900

    카테고리 컨트롤러 생성
    계좌내역 카테고리에 관련 요청을 처리하고 그에 따른 응답을 담당하는
    컨트롤러 입니다.
    
    - Controller.create() 메서드 생성, 이에 따른 테스트 작성.
    - Appclication 정상 작동 및 카테고리 신규 생성 테스트 통과.

--- app/src/main/java/com/codesoom/project/web/controller/CategoryController.java
+++ app/src/main/java/com/codesoom/project/web/controller/CategoryController.java
@@ -1,8 +1,17 @@
 package com.codesoom.project.web.controller;
 
 import com.codesoom.project.core.application.CategoryService;
+import com.codesoom.project.core.domain.Category;
+import com.codesoom.project.web.dto.CategoryRegistrationData;
+import com.codesoom.project.web.dto.CategoryResultData;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * 계좌내역 카테고리에 관련 요청을 처리하고 그에 따른 응답을 담당합니다.
@@ -14,5 +23,29 @@ public class CategoryController {
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
+    }
+
+    /**
+     * 신규 카테고리를 등록하고, 등록한 카테고리을 반환합니다.
+     *
+     * @param registrationData 신규 카테고리 정보
+     * @return 등록한 카테고리
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    CategoryResultData create(@RequestBody @Valid CategoryRegistrationData registrationData) {
+        Category category = categoryService.createCategory(registrationData);
+        return getCategoryResultData(category);
+    }
+
+    private CategoryResultData getCategoryResultData(Category category) {
+        if (category == null) {
+            return null;
+        }
+
+        return CategoryResultData.builder()
+                .id(category.getId())
+                .name(category.getName())
+                .build();
     }
 }
