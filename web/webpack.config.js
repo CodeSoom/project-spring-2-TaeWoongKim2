368c9fe84319b29eea1ecfef6f2b4d181ad1b5de
TaeWoongKim2 <ktaewoong46@gmail.com>
2021-07-18 01:34:44 +0900

    리액트 프로젝트 환경 설정.
    기본적인 설정들을 가져와서 `npm install` 실행했습니다.
    발생하는 버그들을 고치고, simple-react-app 이 실행되도록 만들었습니다.
    
    - Webpack 설정.
    - ESLint 및 Babel 설정.
    - JEST 설정.

---
+++ web/webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        exclude: /node_modules/,
+        use: [
+          'file-loader?name=[name].[ext]',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+      favicon: './public/favicon.ico',
+    }),
+  ],
+};
